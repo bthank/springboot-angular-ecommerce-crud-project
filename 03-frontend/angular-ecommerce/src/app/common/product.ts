e6e98bfe3138eea80e784c5e3fa38b8c9f9a4176
bthank <bthankachan@yahoo.com>
2021-07-23 23:31:38 -0400

    Added Typescript class Product

---
+++ 03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+    // Typescript class for Product
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdated: Date;
+
+}
