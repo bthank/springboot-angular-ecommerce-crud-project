1bcba5c4ab03e68db15d6430c9b1fda56b1a7b04
bthank <bthankachan@yahoo.com>
2021-07-24 00:01:46 -0400

    Added product.service.ts changes for REST processing

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Product } from '../common/product';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'; // from rxjs which is reactive javaScript library
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  // url for SpringBoot REST api
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  // inject the   HttpClient using Angular's injection framework
+  constructor(private httpClient: HttpClient) { }
+
+  // method to map the JSON data from the Spring Data REST service to Product array
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+// unwraps the JSON from Spring Data REST _embedded entry
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
 }
